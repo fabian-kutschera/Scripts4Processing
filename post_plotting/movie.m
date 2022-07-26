f5a02dcef8ca9d43d37922ce69a752d49dc000ed
fabian-kutschera <fabian.kutschera@gmx.net>
2022-07-26 14:57:29 +0200

    update Notebooks and upload scripts for videos

---
+++ post_plotting/movie.m
@@ -0,0 +1,13 @@
+movieid=VideoWriter('output_combined/ssva');
+movieid.FrameRate=4;
+movieid.Quality=100;
+open(movieid)
+
+
+for time=linspace(0.0,179.9,1800)
+    filename = sprintf('./output_combined/trace_%06.2fs.png', time)
+    writeVideo(movieid,imread(filename));   
+
+end
+
+close(movieid)
