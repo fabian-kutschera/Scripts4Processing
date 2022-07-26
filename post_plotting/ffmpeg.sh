adb69f4b08412689f6136bb50ba778e9a0697748
fabian-kutschera <fabian.kutschera@gmx.net>
2022-07-26 18:48:20 +0200

    correct typo

--- post_plotting/ffmpeg.sh
+++ post_plotting/ffmpeg.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-ffmpeg -framerate 24 -i output2/trace_%03d.png -vcodec libx264 -pix_fmt yuv420p -crf 10 ssva.mp4
+ffmpeg -framerate 24 -i output2/trace_%03d.png -vcodec libx264 -pix_fmt yuv420p -crf 10 ssvv.mp4
 
 # -crf: quality: the lower the better
