b70b86a980a1439c87780549621d5ba5779076e8
Fabian Kutschera <82662056+fabian-kutschera@users.noreply.github.com>
2023-10-02 15:29:04 -0700

    Update plot_EnergyOutput.py
    https://stackoverflow.com/questions/58235773/nameerror-name-args-is-not-defined

--- plot_EnergyOutput.py
+++ plot_EnergyOutput.py
@@ -3,9 +3,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 import argparse
 
-parser = argparse.ArgumentParser(description="Plot Energy ouput (default: Moment Rate)")
+description = '''Plot Energy ouput (default: Moment Rate)'''
+parser = argparse.ArgumentParser(description=description)
 parser.add_argument("EnergyOutput", help="energy.csv file from your SeisSol simulation")
 parser.add_argument("--Data", nargs="+", metavar=("variable"), default="MR", help="Data to plot. Default: Moment rate (MR). Other available options are Gravitational energy (GE), Acoustic energy (AE), Acoustic kinetic energy (AKE), Elastic kinetic energy (EKE), Elastic energy (EE), Total frictional work (TFW), Static frictional work (SFW), Seismic moment (SM), Plastic moment (PM)")
+args = parser.parse_args()
 
 df = pd.read_csv(args.EnergyOutput)
 
